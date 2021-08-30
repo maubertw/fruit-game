b99798d9504d79a5b256e1836dabec4c23f24d4f
Mary Warrick <mary@Marys-MBP.cable.rcn.com>
2021-08-29 19:46:54 -0500

    added child_process to root

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+const { execFile, spawn, spawnSync } = require('child_process');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  const spawn = spawnSync('ffprobe' , ['../public/images/CoolVideo.mp4', '-show_frames', '-print_format json'],)
+  console.log('data ', spawn.stdout)
   res.render('index', { 
     title: 'Express', 
     msg: 'Mary and her coding',
