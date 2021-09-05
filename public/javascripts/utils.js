87e2a92b880a2cc62b4d15e9cbaf4b940dc1c446
Mary Warrick <mary@Marys-MBP.cable.rcn.com>
2021-09-05 11:48:07 -0500

    broke some functions out of routes

---
+++ public/javascripts/utils.js
@@ -0,0 +1,72 @@
+const { exec, execSync } = require('child_process');
+
+const filterIFrames = (data) => {
+    const iFrames = [];
+    for(let d in data) {
+        if(data[d].pict_type === 'I') {
+            console.log('hi!')
+            iFrames.push(data[d]);
+        }
+    }
+    return iFrames;
+}
+
+const getBestEffortTimestampTime = (iFrames, startIndex) => {
+    return iFrames[startIndex].best_effort_timestamp_time;
+}
+
+
+module.exports = { 
+    filterIFrames,
+    getBestEffortTimestampTime,
+}
+
+
+
+
+
+
+// COME BACK TO THESE AND FIGURE OUT THE PROMISE BUFFER SITUATION
+// Print any errors, and gets JSON parsed stdout or null if it can't be parsed
+// const getChildProcessData = (error, stdout, stderr) => {
+//     if (error) {
+//         console.log(`error: ${error.message}`);
+//     }
+//     if (stderr) {
+//         console.log(`stderr: ${stderr}`);
+//     }
+//     try {
+//         return JSON.parse(stdout)['frames'];
+//     } catch(error) {
+//         console.log('There was an error processing stout: ', error);
+//         return null;
+//     }
+// }
+    
+// Strip the IFrames from the stdout of getChildProcessData
+    
+
+// const getIFramesJSON = async (videoName) => {
+//     const command = `"ffprobe" -show_frames -print_format json ./public/images/${videoName + '.mp4'}`;
+//     const frameData = await execSync(
+//         command,
+//         { maxBuffer: 10240 * 5000 },
+//         getChildProcessData,
+//     );
+//     try {
+//         return filterIFrames(frameData);
+//     } catch(e) {
+//         console.log('There was an error while getting iFrame data: ', e);
+//         return null;
+//     }
+// }
+    
+// const getGop = () => {
+
+// }
+
+// // Get MP4 of one timestamp to the other
+// const getClip = (index) => {
+
+// }
+    
--- routes/videos.js
+++ routes/videos.js
@@ -1,6 +1,9 @@
 let express = require('express');
 let router = express.Router();
 const { exec } = require('child_process');
+const utilFunctions = require('../public/javascripts/utils');
+
+
 
 // Refrence only
   // res.render('videos', { 
@@ -25,49 +28,51 @@ router.get('/', function(req, res, next) {
 });
 
 // this is writting a single file to disk so far from timestamp to timestamp
-router.get('/:videoName.mp4/group-of-pictures/:groupIndex.mp4', function(req, res, next) {
-  const command = `"ffprobe" -show_frames -print_format json ./public/images/${req.params.videoName + '.mp4'}`
-  exec(
-    command, 
-    {maxBuffer: 10240 * 5000}, 
-    (error, stdout, stderr) => {
-      if (error) {
-          console.log(`error: ${error.message}`);
-      }
-      if (stderr) {
-          console.log(`stderr: ${stderr}`);
-      }
-      const data = JSON.parse(stdout.toString())['frames']
-      const iFrames = []
-      // TODO: GET THE CORRECT FILTERING METHOD TO REDUCE TIME/SPACE
-      for(let d in data) {
-        if(data[d].pict_type == "I") {
-          console.log('times ', data[d].best_effort_timestamp_time)
-          iFrames.push(data[d])
-        }
-      }
-      const frame = req.params.groupIndex
-      const start = iFrames[frame].best_effort_timestamp_time
-      const end = iFrames[frame+1].best_effort_timestamp_time
-      console.log('start end ', start, end)
+// router.get('/:videoName.mp4/group-of-pictures/:groupIndex.mp4', function(req, res, next) {
+//   const command = `"ffprobe" -show_frames -print_format json ./public/images/${req.params.videoName + '.mp4'}`
+//   exec(
+//     command, 
+//     {maxBuffer: 10240 * 5000},
+//     utilFunctions.getChildProcessData 
+//   )
+//     // (error, stdout, stderr) => {
+//     //   if (error) {
+//     //       console.log(`error: ${error.message}`);
+//     //   }
+//     //   if (stderr) {
+//     //       console.log(`stderr: ${stderr}`);
+//     //   }
 
-      const frameCommand = `ffmpeg -ss ${"00:"+start} -to ${"00:"+end} -i ./public/images/CoolVideo.mp4 -c copy ./public/images/output6.mp4`
-      exec(frameCommand, 
-        {maxBuffer: 10240*5000},
-        (error, stdout, stderr) => {
-          if(error) {
-            console.log(`error ${error.message}`);
-          }
-          if(stderr) {
-            console.log(`stderr: ${stderr}`);
-          }
-          console.log('std out', stdout)
-        })
+//     //   const data = JSON.parse(stdout.toString())['frames']
+//     //   const iFrames = []
+//     //   // TODO: GET THE CORRECT FILTERING METHOD TO REDUCE TIME/SPACE
+//     //   for(let d in data) {
+//     //     if(data[d].pict_type == "I") {
+//     //       console.log('times ', data[d].best_effort_timestamp_time)
+//     //       iFrames.push(data[d])
+//     //     }
+//     //   }
+//       const frame = req.params.groupIndex
+//       const start = iFrames[frame].best_effort_timestamp_time
+//       const end = iFrames[frame+1].best_effort_timestamp_time
+
+//       const frameCommand = `ffmpeg -ss ${"00:"+start} -to ${"00:"+end} -i ./public/images/CoolVideo.mp4 -c copy ./public/images/output6.mp4`
+//       exec(frameCommand, 
+//         {maxBuffer: 10240*5000},
+//         (error, stdout, stderr) => {
+//           if(error) {
+//             console.log(`error ${error.message}`);
+//           }
+//           if(stderr) {
+//             console.log(`stderr: ${stderr}`);
+//           }
+//           console.log('std out', stdout)
+//         })
      
-      res.send(stdout)
-  });
+//       res.send(stdout)
+//   });
 
-})
+// })
 
 router.get('/:videoName.mp4/group-of-pictures', function(req, res, next) {
   // Figure out how to serve these files to the client
@@ -90,17 +95,16 @@ router.get('/:videoId.mp4/group-of-pictures.json', function(req, res, next) {
           console.log(`stderr: ${stderr}`);
       }
       const data = JSON.parse(stdout.toString())['frames']
-      const iFrames = []
-      // TODO: GET THE CORRECT FILTERING METHOD TO REDUCE TIME/SPACE
-      for(let d in data) {
-        if(data[d].pict_type == "I") {
-          iFrames.push(data[d])
-        }
-      }
-      res.send(iFrames)
+      res.send(utilFunctions.filterIFrames(data));
   });
 });
 
 
 
 module.exports = router;
+
+
+
+
+
+
