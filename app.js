d3cdf188b051f69c8ec6685a3c1fcbdb5372587d
Mary Warrick <mary@Marys-MBP.cable.rcn.com>
2021-09-30 08:44:15 -0500

    fixed errors

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var videosRouter = require('./routes/videos');
 
 
@@ -23,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/videos', videosRouter);
 
 
